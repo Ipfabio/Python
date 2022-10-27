2ac896fad2d1c321f0acee1137827d912302bfa7
Ip.fabio <95320507+Ipfabio@users.noreply.github.com>
2022-10-27 19:25:56 -0300

    Update Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,5 +1,4 @@
-Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+Python 3.10.5
 def esBisiesto(anio):
     if anio % 4 == 0 and anio % 100 != 0 or anio % 400 == 0:
         return f"El {anio} es bisiesto"
@@ -9,5 +8,3 @@ def esBisiesto(anio):
 anio = input("Ingrese el año: ")
 
 print(esBisiesto(int(anio)))
-
-SyntaxError: invalid syntax
