a62086bf4781e183e766465e9e02ccbc13d9fb0b
Ip.fabio <95320507+Ipfabio@users.noreply.github.com>
2022-10-30 22:30:59 -0300

    Add files via upload

---
+++ Ejercicio 8.py
@@ -0,0 +1,11 @@
+"""
+En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+
+Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+"""
+from operaciones import sumar, restar, multiplicar, dividir
+
+print(sumar(2, 2))
+print(restar(2, 2))
+print(multiplicar(2, 2))
+print(dividir(2, 2))
