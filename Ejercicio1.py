8682a87ed42b54b384d3b6a979e67a6a1602c755
Ip.fabio <95320507+Ipfabio@users.noreply.github.com>
2022-11-13 14:31:23 -0300

    Bibliotecas y Funciones Built In
    Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+# Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
+paises = list(map(str, input("Ingrese los paises: ").replace(",","").strip().split()))
+
+ordenar = sorted(paises)
+
+paises = set(ordenar)
+
+print(paises)
